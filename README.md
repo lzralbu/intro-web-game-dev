fb300aa47f8ce50c4e4cf0078dcf2a3abf93cab3
Lázaro Albuquerque <33807434+lzralbu@users.noreply.github.com>
2023-08-22 12:16:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Introdução ao desenvolvimento de jogos para Web
---
+++ main.ts
@@ -0,0 +1,2 @@
+const canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
